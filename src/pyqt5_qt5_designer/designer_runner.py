50d93b27728243955b0a689f93f5bc944fd1b571
Selmen Arous <selmen.arous@gmail.com>
2024-01-20 17:23:35 +0100

    migrated to new name and strategy

--- src/pyqt5_qt5_designer/designer_runner.py
+++ src/pyqt5_qt5_designer/designer_runner.py
@@ -3,22 +3,22 @@ def main_function():
     import subprocess
 
     # Specify the path to your DLLs folder
-    dlls_folder = os.path.join(os.path.dirname(__file__), "..\\PyQt5\\Qt5\\bin")
-    designer_location =os.path.join(os.path.dirname(__file__) , "bin\designer.exe")
+    #dlls_folder = os.path.join(os.path.dirname(__file__), "..\\PyQt5\\Qt5\\bin")
+    designer_location =os.path.join(os.path.dirname(__file__) , "..\\PyQt5\\Qt5\\bin\\designer.exe")
     #print(dlls_folder)
 
     # Get the current PATH
-    current_path = os.environ.get("PATH", "")
+    #current_path = os.environ.get("PATH", "")
     #print(current_path)
 
 
     # Add the DLLs folder to the PATH
-    os.environ["PATH"] = f"{dlls_folder};{current_path}"
+    #os.environ["PATH"] = f"{dlls_folder};{current_path}"
 
     # Run your executable
     #print("Current Working Directory:", os.getcwd())
     subprocess.run([designer_location])
-    input()
+    #input()
 
 if __name__ == "__main__":
-    main_function()
+    main_function() 
