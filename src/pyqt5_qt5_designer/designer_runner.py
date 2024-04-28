a4ac1872e7abe04f841c20ca537cd3d4475c04f1
Selmen Arous <selmen.arous@gmail.com>
2024-04-28 12:02:24 +0100

    ...

--- src/pyqt5_qt5_designer/designer_runner.py
+++ src/pyqt5_qt5_designer/designer_runner.py
@@ -1,18 +1,19 @@
 import os
 import subprocess
-
-def main_function(*args):
+import sys
+def main_function():
     # Specify the path to the designer executable
     designer_location = os.path.join(os.path.dirname(__file__), "..\\PyQt5\\Qt5\\bin\\designer.exe")
 
+
     # Run designer executable with the provided arguments
-    print("Command:", [designer_location] + list(args))
-    subprocess.run([designer_location] + list(args))
+    print("Command:", [designer_location] + list(sys.argv))
+    subprocess.run([designer_location] + list(sys.argv))
 
 if __name__ == "__main__":
-    import sys
+    
     # Pass any command-line arguments to the main_function
 
     print("sys argv:",sys.argv)
-    main_function(*sys.argv)
+    main_function()
     
