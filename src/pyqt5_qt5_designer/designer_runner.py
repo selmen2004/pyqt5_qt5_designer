50f89f101311600c98279f913528494ebf42bc21
Selmen Arous <selmen.arous@gmail.com>
2024-04-28 22:34:22 +0100

    fix designer not running when no ui loaded

--- src/pyqt5_qt5_designer/designer_runner.py
+++ src/pyqt5_qt5_designer/designer_runner.py
@@ -8,7 +8,13 @@ def main_function():
 
     # Run designer executable with the provided arguments
     #print("Command:", [designer_location] + list(sys.argv))
-    subprocess.run([designer_location] + [sys.argv[1]])
+
+    # Check if any command-line arguments are provided
+    if len(sys.argv) > 1:
+        subprocess.run([designer_location] + [sys.argv[1]])
+    else:
+        # If no arguments are provided, simply run designer.exe without any additional arguments
+        subprocess.run([designer_location])
 
 if __name__ == "__main__":
     
